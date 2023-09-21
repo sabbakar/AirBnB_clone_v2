7c3cc34561829651c117a40e7c5e6827d25c2b38
sabbakar <sadeeqsas14@gmail.com>
2023-09-21 01:02:50 +0100

    pushed

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A String that Starts a Flash Web Application. Your web application must be listening on 0.0.0.0, port 5000"""
+
+from flask import Flask, render_template
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Return a given string """
+    return render_template("10-hbnb_filters.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=None)
